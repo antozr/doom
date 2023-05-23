fece7b00a13743f5fccc26f8632958feb04fac22
anto__dev <antonidwm@gmail.com>
2023-05-23 22:50:07 +0100

    ADD raycasting

--- RaycastingGame/player.py
+++ RaycastingGame/player.py
@@ -23,8 +23,8 @@ class Player:
             dy += speed_sin
     
         if keys[pg.K_s]:
-            dx += speed_cos
-            dy += speed_sin
+            dx += -speed_cos
+            dy += -speed_sin
     
         if keys[pg.K_q]:
             dx += speed_sin
@@ -53,9 +53,9 @@ class Player:
             self.y += dy
         
     def draw(self):
-        pg.draw.line(self.game.screen, "yellow", (self.x *100, self.y *100),
-                     (self.x *100 + WIDTH * math.cos(self.angle),
-                      self.y * 100 + WIDTH * math.sin(self.angle)),2)
+        # pg.draw.line(self.game.screen, "yellow", (self.x *100, self.y *100),
+        #              (self.x *100 + WIDTH * math.cos(self.angle),
+        #               self.y * 100 + WIDTH * math.sin(self.angle)),2)
         pg.draw.circle(self.game.screen, 'green', (self.x *100, self.y * 100), 15)
     
     def update(self):
