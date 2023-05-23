1329b624421997d3b343f08d440450984061f3c0
anto__dev <antonidwm@gmail.com>
2023-05-23 21:31:16 +0100

    add first render map

---
+++ RaycastingGame/player.py
@@ -0,0 +1,27 @@
+from settings import *
+import pygame as pg 
+import math
+
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+        
+    def movement(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED
+    
+    def update(self):
+        self.movement()
+        
+    @property
+    def pos(self):
+        return self.x, self.y
+    
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
