001107cbb89285b7a108a596e26869fcf854a82e
anto__dev <antonidwm@gmail.com>
2023-05-24 23:39:03 +0100

    ADD random ncp pos

--- RaycastingGame/npc.py
+++ RaycastingGame/npc.py
@@ -49,14 +49,13 @@ class NPC(AnimatedSprite):
             self.x += dx
         if self.check_wall(int(self.x), int(self.y + dy * self.size)):
             self.y += dy
-            
+
     def attack(self):
         if self.animation_trigger:
             self.game.sound.npc_shot.play()
             if random() < self.accuracy:
                 self.game.player.get_damage(self.attack_damage)
-            
-            
+
     def animate_death(self):
         if not self.alive:
             if self.game.global_trigger and self.frame_counter < len(self.death_images) - 1:
@@ -91,11 +90,11 @@ class NPC(AnimatedSprite):
                 self.animate_pain()
             elif self.ray_cast_value:
                 self.player_search_trigger = True
-                
+
                 if self.dist < self.attack_dist:
                     self.animate(self.attack_images)
                     self.attack()
-                else:   
+                else:
                     self.animate(self.walk_images)
                     self.movement()
             elif self.player_search_trigger:
@@ -181,3 +180,31 @@ class NPC(AnimatedSprite):
         if self.ray_cast_player_npc():
             pg.draw.line(self.game.screen, 'orange', (100*self.game.player.x, 100*self.game.player.y),
                          (100*self.x, 100*self.y), 2)
+
+
+class SoldierNPC(NPC):
+    def __init__(self, game, path='resources/sprites/npc/soldier/0.png', pos=(10.5, 5.5),
+                 scale=0.6, shift=0.38, animation_time=180):
+        super().__init__(game, path, pos, scale, shift, animation_time)
+
+
+class CacoDemonNPC(NPC):
+    def __init__(self, game, path='resources/sprites/npc/caco_demon/0.png', pos=(10.5, 6.5),
+                 scale=0.7, shift=0.27, animation_time=250):
+        super().__init__(game, path, pos, scale, shift, animation_time)
+        self.attack_dist = 1.0
+        self.health = 150
+        self.attack_damage = 25
+        self.speed = 0.05
+        self.accuracy = 0.35
+
+
+class CyberDemonNPC(NPC):
+    def __init__(self, game, path='resources/sprites/npc/cyber_demon/0.png', pos=(11.5, 6.0),
+                 scale=1.0, shift=0.04, animation_time=210):
+        super().__init__(game, path, pos, scale, shift, animation_time)
+        self.attack_dist = 6
+        self.health = 350
+        self.attack_damage = 15
+        self.speed = 0.055
+        self.accuracy = 0.25
--- RaycastingGame/object_handler.py
+++ RaycastingGame/object_handler.py
@@ -1,7 +1,6 @@
 from sprite_object import *
 from npc import *
-
-
+from random import choices, randrange
 
 class ObjectHandler:
     def __init__(self, game):
@@ -15,7 +14,14 @@ class ObjectHandler:
         add_npc = self.add_npc
         self.npc_positions = {}
         
-        #sprite map
+        # spawn npc
+        self.enemies = 20  # npc count
+        self.npc_types = [SoldierNPC, CacoDemonNPC, CyberDemonNPC]
+        self.weights = [70, 20, 10]
+        self.restricted_area = {(i, j) for i in range(10) for j in range(10)}
+        self.spawn_npc()
+
+        # sprite map
         add_sprite(SpriteObject(game))
         add_sprite(AnimatedSprite(game))
         add_sprite(AnimatedSprite(game, pos=(1.5, 1.5)))
@@ -23,23 +29,44 @@ class ObjectHandler:
         add_sprite(AnimatedSprite(game, pos=(5.5, 6.5)))
         add_sprite(AnimatedSprite(game, pos=(2.5, 3.5)))
         add_sprite(AnimatedSprite(game, pos=(5.5, 4.5)))
-        add_sprite(AnimatedSprite(game, pos=(10.5, 6.5), path=self.anim_sprite_path + 'red_light/0.png'))
-        add_sprite(AnimatedSprite(game, pos=(15.5, 2.5), path=self.anim_sprite_path + 'red_light/0.png'))
-        add_sprite(AnimatedSprite(game, pos=(2.5, 8.5), path=self.anim_sprite_path + 'red_light/0.png'))
-        add_sprite(AnimatedSprite(game, pos=(13.5, 0.5), path=self.anim_sprite_path + 'red_light/0.png'))
-        
-        #npc map
-        add_npc(NPC(game))
-        add_npc(NPC(game, pos=(11.5, 4.5)))
-        
-        
+        add_sprite(AnimatedSprite(game, pos=(10.5, 6.5),
+                   path=self.anim_sprite_path + 'red_light/0.png'))
+        add_sprite(AnimatedSprite(game, pos=(15.5, 2.5),
+                   path=self.anim_sprite_path + 'red_light/0.png'))
+        add_sprite(AnimatedSprite(game, pos=(2.5, 8.5),
+                   path=self.anim_sprite_path + 'red_light/0.png'))
+        add_sprite(AnimatedSprite(game, pos=(13.5, 0.5),
+                   path=self.anim_sprite_path + 'red_light/0.png'))
+
+        # npc map
+        # add_npc(NPC(game))
+        # add_npc(NPC(game, pos=(11.5, 4.5)))
+
+    def spawn_npc(self):
+        for i in range(self.enemies):
+            npc = choices(self.npc_types, self.weights)[0]
+            pos = x, y = randrange(
+                self.game.map.cols), randrange(self.game.map.rows)
+            while (pos in self.game.map.world_map) or (pos in self.restricted_area):
+                pos = x, y = randrange(
+                    self.game.map.cols), randrange(self.game.map.rows)
+            self.add_npc(npc(self.game, pos=(x + 0.5, y + 0.5)))
+
+    def check_win(self):
+        if not len(self.npc_positions):
+            self.game.object_renderer.win()
+            pg.display.flip()
+            pg.time.delay(1500)
+            self.game.new_game()
+
     def update(self):
-        self.npc_positions = {npc.map_pos for npc in self.npc_list if npc.alive}
+        self.npc_positions = {
+            npc.map_pos for npc in self.npc_list if npc.alive}
         [sprite.update() for sprite in self.sprite_list]
         [npc.update() for npc in self.npc_list]
-        
+
     def add_npc(self, npc):
         self.npc_list.append(npc)
-        
+
     def add_sprite(self, sprite):
         self.sprite_list.append(sprite)
